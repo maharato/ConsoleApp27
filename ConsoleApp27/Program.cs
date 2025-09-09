a9cb3530e5c6527144118e6e19ced2be95e2ec9d
maharato <ahmedmostafa565565@gmail.com>
2025-09-10 00:10:43 +0300

    a

--- ConsoleApp27/Program.cs
+++ ConsoleApp27/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Threading.Channels;
 
 namespace ConsoleApp27
 {
@@ -13,7 +14,13 @@ namespace ConsoleApp27
         List<string> easyQuestion_correct = new List<string>();
         List<string> mediumQuestion_correct = new List<string>();
         List<string> hardQuestion_correct = new List<string>();
-        int marks = 0;
+
+        List<int> easy_marks = new List<int>();
+        List<int> medium_marks = new List<int>();
+        List<int> hard_marks = new List<int>();
+
+
+
         public string questionTypee(int type)
         {
             switch (type)
@@ -28,35 +35,34 @@ namespace ConsoleApp27
                     return "unknown";
             }
         }
-        public string questionLevel(int level)
-        {
-            switch (level)
-            {
-                case 1:
-                    return "Easy";
-                case 2:
-                    return "Medium";
-                case 3:
-                    return "Hard";
-                default:
-                    return "unknown";
-            }
+        public List<string>examquestypee(int type) {
+            if (type==1) { return easyQuestion; }
+            if (type == 2) { return mediumQuestion; }
+            if (type == 3) { return hardQuestion; }
+            else return easyQuestion;
+
         }
+      
         public void addQuestion(string question, int level, string correctAnswer,int marks)
         {
+
             switch (level)
             {
                 case 1:
                     easyQuestion.Add(question);
                     easyQuestion_correct.Add(correctAnswer);
+                    easy_marks.Add(marks);
                     break;
                 case 2:
                     mediumQuestion.Add(question);
                     mediumQuestion_correct.Add(correctAnswer);
+                    medium_marks.Add(marks);
+
                     break;
                 case 3:
                     hardQuestion.Add(question);
                     hardQuestion_correct.Add(correctAnswer);
+                    hard_marks.Add(marks);
                     break;
                 default:
                     Console.WriteLine("Invalid level");
@@ -75,35 +81,102 @@ namespace ConsoleApp27
     {
         static void Main(string[] args)
         {
-            while (true)
+            bool exit = false;
+            questions ques = new questions();
+            Console.WriteLine(" please chooce your role 1 doctor " +
+                "2 student");
+            int role = Convert.ToInt32(Console.ReadLine());
+            if (role == 1) {
+            while (exit==false)
             {
+                
+
+                  
 
 
-                questions ques = new questions();
-                Console.WriteLine("please enter number of question");
+                    Console.WriteLine("please enter number of question");
                 int numQuestions = Convert.ToInt32(Console.ReadLine());
-                for (int i = 0; i < numQuestions; i++)
-                {
-                    Console.WriteLine("please enter question type (1: True/False, 2: Choose One, 3: Multiple Choice)");
-                    int type = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("please enter question level");
-                    Console.WriteLine("1: Easy, 2: Medium, 3: Hard");
-                    int level = Convert.ToInt32(Console.ReadLine());
+                    for (int i = 1; i <= numQuestions; i++)
+                    {
+                        while (i==numQuestions)
+                        {
+                         exit = true;
+                        }
+
+                        string question = "";
+                        Console.WriteLine("please enter question type (1: True/False, 2: Choose One, 3: Multiple Choice)");
+                        int type = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("please enter question level");
+                        Console.WriteLine("1: Easy, 2: Medium, 3: Hard");
+                        int level = Convert.ToInt32(Console.ReadLine());
 
 
 
-                    Console.WriteLine("please enter the question");
-                    string question = Console.ReadLine();
-                    Console.WriteLine("please enter the question marks");
-                    int questionMarks = Convert.ToInt32(Console.ReadLine());
-                    if () { 
-                    
-                    
+
+
+                        if (type == 1)
+                        {
+                            Console.WriteLine("please enter the question");
+                            question = Console.ReadLine();
+                            Console.WriteLine("please enter the question marks");
+                            int questionMarks = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("please enter correct answer");
+                            string correctAnswer = Console.ReadLine();
+                            ques.addQuestion(question, level, correctAnswer, questionMarks);
+
+                        }
+                        else if (type == 2)
+                        {
+                            Console.WriteLine("please enter the question");
+                            for (i = 0; i <= 4; i++)
+                            {
+                                if (i >= 1) { Console.WriteLine("enter choice " + i); }
+                                question = question + "/n" + Console.ReadLine();
+                            }
+                            Console.WriteLine("please enter the question marks");
+                            int questionMarks = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("please enter correct answer number");
+
+                            string correctAnswer = Convert.ToString(Console.ReadLine());
+                            ques.addQuestion(question, level, correctAnswer, questionMarks);
+                        }
+                        else if (type == 3)
+                        {
+                            for (i = 0; i <= 4; i++)
+                            {
+                                if (i >= 1) { Console.WriteLine("enter choice " + i); }
+                                question = question + "/n" + Console.ReadLine();
+                            }
+
+
+                            Console.WriteLine("please enter the question marks");
+                            int questionMarks = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("please enter correct answer");
+                            string correctAnswer = Console.ReadLine();
+                            ques.addQuestion(question, level, correctAnswer, questionMarks);
+                        }
                     }
+                 
 
 
 
                 }
+            }
+            else
+            {
+                Console.WriteLine("please chooce the type of the exam 1 practical 2 final");
+
+                Console.WriteLine("please chooce the type of exam 1 Easy, 2 Medium,3 Hard");
+                int type = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < ques.questionTypee(type).Length; i++) {
+                    Console.WriteLine(ques.questionTypee(type)[i]);
+                }
+
+ 
+
+
+
             }
         }
     }
